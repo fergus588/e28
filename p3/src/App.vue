0cc46aa4a5c0237a0cd2fbc13b0c0769c1f0cf4f
Fergus Cameron <fergus@longstonetyres.co.uk>
2019-11-28 18:58:13 +0000

    about to add api

---
+++ p3/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <nav class="side-menu">
+      <ul>
+        <li>
+            <router-link to='/'>Home</router-link>
+        </li>
+        <li>
+            <router-link to='/alfa'>Alfa Romeo</router-link>
+        </li>
+        <li>
+            <router-link to='/ferrari'>Ferrari</router-link>
+        </li>
+      </ul>
+    </nav>
+    <div class="main-content">
+    <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'app',
+  components: {},
+  data: function(){
+    return{
+    }
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.side-menu{
+  width:20%;
+  position: absolute;
+  left: 0;
+  background:#dedede;
+}
+.main-content{
+  width: 73%;
+  margin-left: 22%;
+  right:0;
+  padding: 0 20px;
+  font-size: 20px;
+}
+</style>
